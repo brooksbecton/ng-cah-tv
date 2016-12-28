66b6d51db0597678c1574f799bfb915f7e548291
Adincath <broabect@ut.utm.edu>
2016-12-28 08:38:08 -0600

    Fixed Routing issue with links in game module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-// Imports commented out for brevity
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { GameComponent } from './game.component'
 import { CardComponent } from './card/card.component';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     CardComponent,
